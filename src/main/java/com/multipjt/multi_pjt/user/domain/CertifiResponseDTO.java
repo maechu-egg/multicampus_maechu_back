40c77e726e07d69f1d5a87558fa8467607cc5f3e
leeyeonju02 <dlduswn0783@naver.com>
2024-10-13 19:11:33 +0900

    feat: 유저테이블 dto 정의

---
+++ src/main/java/com/multipjt/multi_pjt/user/domain/CertifiResponseDTO.java
@@ -0,0 +1,10 @@
+package com.multipjt.multi_pjt.user.domain;
+
+import lombok.Data;
+
+@Data
+public class CertifiResponseDTO {
+    private String user_id;
+    private String email;
+    private String certification_number;
+}
