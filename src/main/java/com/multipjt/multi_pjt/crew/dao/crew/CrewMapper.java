a86431b4dbbef186f406b0c20cb8ad7a7bd1c580
Yuminyumin <102581107+Yuminyumin@users.noreply.github.com>
2024-10-16 13:33:30 +0900

    test : 001 수정중

--- src/main/java/com/multipjt/multi_pjt/crew/dao/crew/CrewMapper.java
+++ src/main/java/com/multipjt/multi_pjt/crew/dao/crew/CrewMapper.java
@@ -1,11 +1,16 @@
 package com.multipjt.multi_pjt.crew.dao.crew;
 
 import org.apache.ibatis.annotations.Mapper;
+import java.util.List;
 
 import com.multipjt.multi_pjt.crew.domain.crew.CrewRequestDTO;
+import com.multipjt.multi_pjt.crew.domain.crew.CrewResponseDTO;
 
 @Mapper
 public interface CrewMapper {
     // 크루 생성
-    public void createCrew(CrewRequestDTO param);
+    public void createCrewRow(CrewRequestDTO param);
+
+    // crew_id로 크루 조회
+    public List<CrewResponseDTO> selectCrewRow();
 }
--- src/test/java/com/multipjt/multi_pjt/crew/MybatisApplicationTests.java
+++ src/test/java/com/multipjt/multi_pjt/crew/MybatisApplicationTests.java
@@ -1,5 +1,9 @@
 package com.multipjt.multi_pjt.crew;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,19 +11,23 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.multipjt.multi_pjt.crew.dao.crew.CrewMapper;
 import com.multipjt.multi_pjt.crew.domain.crew.CrewRequestDTO;
+import com.multipjt.multi_pjt.crew.domain.crew.CrewResponseDTO;
+
+import java.util.List;
 
 @SpringBootTest
-@Transactional
+// @Transactional
 public class MybatisApplicationTests {
     
     @Autowired
     private CrewMapper crewMapper;
 
     @Test
+    @DisplayName("001 : 크루 생성 테스트")
     public void createCrewTest(){
         //given : 크루 생성에 필요한 크루 정보
         CrewRequestDTO request = CrewRequestDTO.builder()
-                                    .crew_id(3)
+                                    .crew_id(5)
                                     .member_id("testuser")
                                     .crew_name("testCrew")
                                     .crew_goal("다이어트")
@@ -31,7 +39,27 @@ public class MybatisApplicationTests {
                                     .crew_age("20대")
                                     .crew_date("2024-10-15 17:25:10")
                                     .build();
-        //then : 크루 
-        crewMapper.createCrew(request);
+        //when : 크루 생성
+        crewMapper.createCrewRow(request);
+
+        //then : 데이터베이스에서 등록된 크루 검증
+        List<CrewResponseDTO> createdCrew = crewMapper.selectCrewRow();
+        System.out.println("조회된 크루 : "+ createdCrew);
+        //조회된 크루 : []
+        assertThat(createdCrew).isNotNull()        .withFailMessage("크루가 생성되어야 합니다.");
+        assertThat(createdCrew.size()).isGreaterThan(0).withFailMessage("크루가 생성되어야 합니다."); // 크루가 생성되었는지 확인
+
+        // // 첫 번째 크루에 대한 검증
+        // CrewResponseDTO crew = createdCrew.get(0);
+        // assertThat(crew.getMember_id())     .isEqualTo("testuser")                .withFailMessage("크루 생성자가 일치해야 합니다.");
+        // assertThat(crew.getCrew_name())     .isEqualTo("testCrew")                .withFailMessage("크루 이름이 일치해야 합니다.");
+        // assertThat(crew.getCrew_goal())     .isEqualTo("다이어트")                 .withFailMessage("크루 목표가 일치해야 합니다.");
+        // assertThat(crew.getCrew_title())    .isEqualTo("다이어트 목표 크루원 모집") .withFailMessage("크루 제목이 일치해야 합니다.");
+        // assertThat(crew.getCrew_location()) .isEqualTo("익산")                    .withFailMessage("크루 위치가 일치해야 합니다.");
+        // assertThat(crew.getCrew_sport())    .isEqualTo("런닝")                    .withFailMessage("크루 스포츠가 일치해야 합니다.");
+        // assertThat(crew.getCrew_gender())   .isEqualTo("혼성")                    .withFailMessage("크루 성별이 일치해야 합니다.");
+        // assertThat(crew.getCrew_frequency()).isEqualTo("주 3회")                  .withFailMessage("크루 빈도가 일치해야 합니다.");
+        // assertThat(crew.getCrew_age())      .isEqualTo("20대")                    .withFailMessage("크루 연령대가 일치해야 합니다.");
+        // assertThat(crew.getCrew_date())     .isEqualTo("2024-10-15 17:25:10")     .withFailMessage("크루 날짜가 일치해야 합니다.");
     }
 }
