3dee950ac2353fe08029a2c71bc750140c28c5c6
camelliaseolwang <toblossommoon@gmail.com>
2024-10-18 07:14:37 +0900

    community - dao - PostMapper add

---
+++ src/main/java/com/multipjt/multi_pjt/community/dao/PostMapper.java
@@ -0,0 +1,23 @@
+package com.multipjt.multi_pjt.community.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.multipjt.multi_pjt.community.domain.posts.PostRequestDTO;
+import com.multipjt.multi_pjt.community.domain.posts.PostResponseDTO;
+
+
+
+@Mapper
+public interface PostMapper {
+    
+    public int postInsert(PostRequestDTO pdto);
+
+     //  member_id로 게시글 조회
+    List<PostResponseDTO> getMemberById(@Param("member_id") int mid);
+
+    
+
+}
---
+++ src/main/java/com/multipjt/multi_pjt/community/dao/commentMapper.java
@@ -0,0 +1,12 @@
+package com.multipjt.multi_pjt.community.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface commentMapper {
+
+    public int commentInsert();
+
+
+
+}
