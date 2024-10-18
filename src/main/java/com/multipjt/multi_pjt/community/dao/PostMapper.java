1b0d8e437043d1543fb44a7f39486b2f5a253d6c
camelliaseolwang <toblossommoon@gmail.com>
2024-10-18 11:38:46 +0900

    annotation

--- src/main/java/com/multipjt/multi_pjt/community/dao/PostMapper.java
+++ src/main/java/com/multipjt/multi_pjt/community/dao/PostMapper.java
@@ -13,17 +13,19 @@ import com.multipjt.multi_pjt.community.domain.posts.PostResponseDTO;
 @Mapper
 public interface PostMapper {
     
+    // 게시글 등록
     public int postInsert(PostRequestDTO pdto);
 
      //  member_id로 게시글 조회
     List<PostResponseDTO> getMemberById(@Param("member_id") int mid);
 
-    
+    // 게시글 수정
     public int postUpdate(PostResponseDTO pdto);
 
-
+    // 게시글 번호로 조회
     PostResponseDTO getPostById(@Param("post_id") int pid);
 
+    // 운동 종목 별 게시글 조회
     List<PostResponseDTO> postSelectSport(@Param("post_sport") String psport);
     
 
