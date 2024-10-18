17c505b1879c424f089d3f38a718205441db1375
camelliaseolwang <toblossommoon@gmail.com>
2024-10-18 16:45:21 +0900

    add Comment DTO

---
+++ src/main/java/com/multipjt/multi_pjt/community/domain/comments/CommentRequestDTO.java
@@ -0,0 +1,17 @@
+package com.multipjt.multi_pjt.community.domain.comments;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class CommentRequestDTO {
+    private int     comments_id;
+    private String  comments_contents;
+    private Date    comments_date;
+    
+    // 외래키 
+    private int     member_id;
+    private int     post_id;
+
+}
---
+++ src/main/java/com/multipjt/multi_pjt/community/domain/comments/CommentResponseDTO.java
@@ -0,0 +1,16 @@
+package com.multipjt.multi_pjt.community.domain.comments;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class CommentResponseDTO {
+    private int     comments_id;
+    private String  comments_contents;
+    private Date    comments_date;
+    
+    // 외래키 
+    private int     member_id;
+    private int     post_id;
+}
