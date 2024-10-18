c475581a7758221639ed54429e0f30e4b455386c
seokhun.han <seokhun0906@gmail.com>
2024-10-18 10:38:38 +0900

    test : 003 에러

--- src/main/java/com/multipjt/multi_pjt/record/diet/dao/DietMapper.java
+++ src/main/java/com/multipjt/multi_pjt/record/diet/dao/DietMapper.java
@@ -4,15 +4,23 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.multipjt.multi_pjt.record.diet.domain.DietRequestDTO;
 import com.multipjt.multi_pjt.record.diet.domain.ItemRequestDTO;
+import com.multipjt.multi_pjt.record.diet.domain.ItemResponseDTO;
 
+import java.util.ArrayList;
 @Mapper
 public interface DietMapper {
     
+    // DietRecords 삽입
     public int dietInsert(DietRequestDTO requestDTO);
-
+    // DietItems 삽입
     public int itemInsert(ItemRequestDTO requestDTO);
- 
+    // diet_id 찾기
     public Long findDietNumber(DietRequestDTO requestDTO);
-
+    // DietRecords 삭제
     public int dietDelete(Long dietId);
+    // DietItems 삭제
+    public int itemDelete(Long itemid);
+
+    public ArrayList<ItemResponseDTO> itemFindAll(Long dietId);
+
 }
--- src/test/java/com/multipjt/multi_pjt/record/DietMapperApplicationTest.java
+++ src/test/java/com/multipjt/multi_pjt/record/DietMapperApplicationTest.java
@@ -6,16 +6,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import org.junit.jupiter.api.DisplayName;
 import java.time.LocalDate;
+import java.util.List;
 
 import com.multipjt.multi_pjt.record.diet.dao.DietMapper;
 import com.multipjt.multi_pjt.record.diet.domain.DietRequestDTO;
 import com.multipjt.multi_pjt.record.diet.domain.ItemRequestDTO;
+import com.multipjt.multi_pjt.record.diet.domain.ItemResponseDTO;
 import com.multipjt.multi_pjt.record.diet.domain.MealType;
 
 @SpringBootTest
-@Transactional
 public class DietMapperApplicationTest {
     
     @Autowired
@@ -59,7 +59,7 @@ public class DietMapperApplicationTest {
     }
 
     @Test
-    @DisplayName("식단 전체 삭제")
+    @DisplayName("002 : 식단 삭제")
     public void testDietDelete(){
     
         Long dietNumber = testDietRecord();
@@ -67,6 +67,44 @@ public class DietMapperApplicationTest {
         int deletRow = dietMapper.dietDelete(dietNumber);
 
         Assertions.assertThat(deletRow).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("003 : 식단 조회")
+    public void testDietFind(){
+    
+        Long dietId = testDietRecord();
+
+        List<ItemResponseDTO> list = dietMapper.itemFindAll(dietId);
+        System.out.println("debug >>> " + list);
+
+        Assertions.assertThat(list).isNotEmpty();
+
+        // 특정 항목에 대한 검증 (예: 첫 번째 항목 확인)
+        Assertions.assertThat(list.get(0).getDietId()).isEqualTo(dietId);
+        Assertions.assertThat(list.get(0).getItemName()).isEqualTo("공기밥");
+
+    }
+    
+    @Test
+    @DisplayName("004 : 식품 삭제")
+    public void testItemDelete(){
+
+        ItemRequestDTO itemRequestDTO = new ItemRequestDTO();
+        itemRequestDTO.setDietId(1L);
+        itemRequestDTO.setCalories(300);
+        itemRequestDTO.setCarbs(30);
+        itemRequestDTO.setFat(30);
+        itemRequestDTO.setItemName("공기밥");
+        itemRequestDTO.setQuantity(100);
+        itemRequestDTO.setProtein(30);
+        //when : db에 값 insert
+        int item = dietMapper.itemInsert(itemRequestDTO);
+
+        Assertions.assertThat(item).isEqualTo(1);
+
+        dietMapper.itemDelete(null);
+
     }
 
 }
