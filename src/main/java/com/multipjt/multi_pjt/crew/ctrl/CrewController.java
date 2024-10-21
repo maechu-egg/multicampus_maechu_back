75e93685f0e1ba41d4f9e89e63584ee6573403cc
Yuminyumin <102581107+Yuminyumin@users.noreply.github.com>
2024-10-21 17:29:31 +0900

    feat: CrewController.java 파일 생성 및 크루 생성 기능 구현

--- src/main/java/com/multipjt/multi_pjt/crew/ctrl/CrewController.java
+++ src/main/java/com/multipjt/multi_pjt/crew/ctrl/CrewController.java
@@ -1,5 +1,28 @@
 package com.multipjt.multi_pjt.crew.ctrl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.multipjt.multi_pjt.crew.domain.crew.CrewRequestDTO;
+import com.multipjt.multi_pjt.crew.service.CrewService;
+
+@RestController
+@RequestMapping("/crew")
 public class CrewController {
     
+    @Autowired
+    private CrewService crewService;
+
+    //크루 생성
+    @PostMapping("/create")
+    public ResponseEntity<Void> createCrew(@RequestBody CrewRequestDTO param) {
+        System.out.println("client endpoint: /crew/create");
+        System.out.println("debug: createCrew + " + param);
+        crewService.createCrew(param);
+        return ResponseEntity.ok().build();
+    }    
 }
