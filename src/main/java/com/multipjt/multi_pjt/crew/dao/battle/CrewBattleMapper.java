839c10e6e9f136a63562439c5c3b69f1da794ad4
Yuminyumin <102581107+Yuminyumin@users.noreply.github.com>
2024-10-21 13:55:31 +0900

    refactor: crewBattleMapper
    기능별 컬럼 부분 수정

--- src/main/java/com/multipjt/multi_pjt/crew/dao/battle/CrewBattleMapper.java
+++ src/main/java/com/multipjt/multi_pjt/crew/dao/battle/CrewBattleMapper.java
@@ -14,18 +14,28 @@ import com.multipjt.multi_pjt.crew.domain.battle.CrewVoteRequestDTO;
 
 @Mapper
 public interface CrewBattleMapper {
+    // <---- 크루 배틀 ---->
 
+    // 배틀 생성
     public void saveCrewBattleRow(CrewBattleRequestDTO params);
     
+    // 배틀 목록 조회
     public List<CrewBattleResponseDTO> selectCrewBattleRow();
 
+    // 배틀 신청
     public void saveBattleMemberRow(BattleMemberRequestDTO params);
 
+    // <---- 크루 배틀 상세보기 ---->
+
+    // 배틀 참가 멤버 조회
     public List<BattleMemberResponseDTO> selectBattleMemberRow();
 
+    // 피드 작성
     public void saveCrewBattleFeedRow(CrewBattleFeedRequestDTO params);
 
+    // 피드 조회
     public List<CrewBattleFeedResponseDTO> selectCrewBattleFeedRow();
 
+    // 투표
     public void saveVoteRow(CrewVoteRequestDTO params);
 } 
