7a27f7e1b3b431a574c2c6ac50fb45c8fc5d2370
seokhun.han <seokhun0906@gmail.com>
2024-10-22 13:46:47 +0900

    refator

--- src/main/java/com/multipjt/multi_pjt/user/ctrl/UserController.java
+++ src/main/java/com/multipjt/multi_pjt/user/ctrl/UserController.java
@@ -2,18 +2,10 @@ package com.multipjt.multi_pjt.user.ctrl;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.multipjt.multi_pjt.user.domain.UserResponseDTO;
 import com.multipjt.multi_pjt.user.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("/user")
