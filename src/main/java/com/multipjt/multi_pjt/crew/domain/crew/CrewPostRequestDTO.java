d6fa14c6aa7dbf4df104f1619e096030666195a6
Yuminyumin <102581107+Yuminyumin@users.noreply.github.com>
2024-10-23 15:45:04 +0900

    fix: DTO에서 조회수 삭제

--- src/main/java/com/multipjt/multi_pjt/crew/domain/crew/CrewPostRequestDTO.java
+++ src/main/java/com/multipjt/multi_pjt/crew/domain/crew/CrewPostRequestDTO.java
@@ -15,7 +15,6 @@ public class CrewPostRequestDTO {
     private String crew_post_content;
     private String crew_post_img;
     private int crew_post_like;
-    private int crew_post_hits;
     private int crew_post_state;
     private String crew_post_date;
     private int crew_id;
