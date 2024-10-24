57d75c5fc34e7d89d2cfe076edd743e4a7dad638
ddayhyun <ddayhyun1121@gmail.com>
2024-10-25 02:27:30 +0900

    feat: 개인뱃지mapper 활동기록  메서드 추가

--- src/main/java/com/multipjt/multi_pjt/Badge/dao/MemberBadgeMapper.java
+++ src/main/java/com/multipjt/multi_pjt/Badge/dao/MemberBadgeMapper.java
@@ -3,20 +3,26 @@ package com.multipjt.multi_pjt.badge.dao;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+import java.util.Map;
+
 import com.multipjt.multi_pjt.badge.domain.badge.MemberBadgeRequestDTO;
 import com.multipjt.multi_pjt.badge.domain.badge.MemberBadgeResponseDTO;
 
 @Mapper
 public interface MemberBadgeMapper {
-     // 뱃지 생성
-     public void insertBadge(MemberBadgeRequestDTO param);
+    // 뱃지 생성
+    public void insertBadge(MemberBadgeRequestDTO param);
     
-     // 특정 회원의 뱃지 조회
-     public MemberBadgeResponseDTO getselectBadgeByMemberId(@Param("member_id") int memberId);
- 
-     // 특정 회원의 뱃지 업데이트
-     public void updateBadge(MemberBadgeRequestDTO param);
+    // 특정 회원의 뱃지 조회
+    public MemberBadgeResponseDTO getBadgeByMemberId(@Param("member_id") int memberId);
 
-    
+    // 특정 회원의 뱃지 업데이트
+    public void updateBadge(MemberBadgeRequestDTO param);
+
+    // 멤버 활동 기록을 조회하는 메서드
+    public List<Map<String, Object>> getMemberActivity(@Param("member_id") int memberId);
+
+    // 활동 기록을 UserActivityRecord 테이블에 삽입하는 메서드
+    public void insertUserActivityRecord(Map<String, Object> activityData);
 }
-    
