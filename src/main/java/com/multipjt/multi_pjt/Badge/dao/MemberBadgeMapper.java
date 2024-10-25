0dd4615719686e40aaeda118466667d01d372ebd
ddayhyun <ddayhyun1121@gmail.com>
2024-10-25 13:04:12 +0900

    feat: 모든회원 개인뱃지 조회

--- src/main/java/com/multipjt/multi_pjt/Badge/dao/MemberBadgeMapper.java
+++ src/main/java/com/multipjt/multi_pjt/Badge/dao/MemberBadgeMapper.java
@@ -1,10 +1,11 @@
 package com.multipjt.multi_pjt.badge.dao;
 
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.multipjt.multi_pjt.badge.domain.badge.MemberBadgeRequestDTO;
 import com.multipjt.multi_pjt.badge.domain.badge.MemberBadgeResponseDTO;
@@ -21,8 +22,15 @@ public interface MemberBadgeMapper {
     public void updateBadge(MemberBadgeRequestDTO param);
 
     // 멤버 활동 기록을 조회하는 메서드
-    public List<Map<String, Object>> getMemberActivity(@Param("member_id") int memberId);
+    public List<Map<String, Object>> getMemberActivity(@Param("member_id") Long memberId);
 
     // 활동 기록을 UserActivityRecord 테이블에 삽입하는 메서드
     public void insertUserActivityRecord(Map<String, Object> activityData);
+
+    // 모든 회원의 뱃지 조회
+    public List<MemberBadgeResponseDTO> getAllBadges();
+
+    BigDecimal getCurrentPoints(@Param("member_id") Long memberId);
 }
+
+
