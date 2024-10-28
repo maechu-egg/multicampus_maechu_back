4c3f84505401127fe4a3e2dbf741c53802e32765
Yuminyumin <102581107+Yuminyumin@users.noreply.github.com>
2024-10-28 16:03:05 +0900

    fix: 배틀 참가 멤버 조회 퀴리문 수정
    join - 닉네임, 지역 ,나이, 피드 수, 총 소모 칼로리, 총 운동시간
    gruop by - 배틀 참여자, 배틀 번호, 멤버 번호 그룹화

--- src/main/java/com/multipjt/multi_pjt/crew/domain/battle/BattleMemberResponseDTO.java
+++ src/main/java/com/multipjt/multi_pjt/crew/domain/battle/BattleMemberResponseDTO.java
@@ -4,7 +4,13 @@ import lombok.Data;
 
 @Data
 public class BattleMemberResponseDTO {
-    private int participant_id;
-    private int battle_id;
-    private int member_id;
+    private Integer participant_id;
+    private Integer battle_id;
+    private Integer member_id;
+    private String  nickname;
+    private String  profile_region;
+    private Integer profile_age;
+    private Integer total_feed_kcal;
+    private Integer total_feed_exTime;
+    private Integer feed_count;
 }
