b19f096bfb3b0e5d9953414606ef97c81f13c140
camelliaseolwang <toblossommoon@gmail.com>
2024-11-04 09:55:25 +0900

    fix comment-ActivityRecord

--- src/main/java/com/multipjt/multi_pjt/community/ctrl/CommentController.java
+++ src/main/java/com/multipjt/multi_pjt/community/ctrl/CommentController.java
@@ -3,7 +3,6 @@ package com.multipjt.multi_pjt.community.ctrl;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.multipjt.multi_pjt.badge.domain.record.UserActivityRecordRequsetDTO;
 import com.multipjt.multi_pjt.community.domain.comments.CommentRequestDTO;
 import com.multipjt.multi_pjt.community.domain.comments.CommentResponseDTO;
 import com.multipjt.multi_pjt.community.service.CommentService;
@@ -41,62 +40,105 @@ public class CommentController {
     @PostMapping("/saveComment")
     public ResponseEntity<String> saveComment(@RequestBody CommentRequestDTO cdto,
                                             @RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
+        
+        
         System.out.println("Controller endpoint : /community/commnet/saveComment");
         System.out.println("cdto - " + cdto);
+        
+        Map<String, Object> response = new HashMap<>();
+        ResponseEntity<String> result = null;
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String token = authHeader.substring(7); // "Bearer " 접두사 제거
             int userId = jwtTokenProvider.getUserIdFromToken(token); // 토큰에서 사용자 ID 추출 (int형으로 변경)
             System.out.println("userId : " + userId);
          
-            UserActivityRecordRequsetDTO uadto = new UserActivityRecordRequsetDTO();
 
-            uadto.setActivity_type("comment");
-            uadto.setMember_id(userId);
-            commentService.createComment(cdto, uadto);
+            Map<String, Object> map = new HashMap<>();
+            map.put("activityType", "comment");
+            map.put("memberId", userId);
+           
+            response = commentService.createComment(cdto, map);
 
-            return ResponseEntity.status(HttpStatus.CREATED).body("댓글이 성공적으로 추가되었습니다");
+            if(response.get("result") != null && (boolean)response.get("result")){
+                result = ResponseEntity.status(HttpStatus.CREATED).body("댓글이 성공적으로 추가되었습니다");
+            }else{
+                result = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("댓글 추가가 실패 했습니다. ");
+            }
+
+            
         }else{
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("로그인 한 사용자만 글쓰기가 가능합니다.");
         }
-        
+        return result;
     }
 
     // 댓글 list
     @GetMapping("/getComment/{postId}")
-    public ResponseEntity<List<CommentResponseDTO>> commentList(@PathVariable("postId") Integer postId) {
+    public ResponseEntity<List<CommentResponseDTO>> commentList(@PathVariable("postId") Integer postId,
+                                                                @RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
         System.out.println("Controller client endpoint : /community/comment/getComment/{postId}" + postId);
         System.out.println("postId - " + postId);
+        List<CommentResponseDTO> list = null;
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7); // "Bearer " 접두사 제거
+            int userId = jwtTokenProvider.getUserIdFromToken(token); // 토큰에서 사용자 ID 추출 (int형으로 변경)
+            System.out.println("userId : " + userId);
         
-        Map<String, Integer> map = new HashMap<>();
-        map.put("post_id" , postId);
+            Map<String, Integer> map = new HashMap<>();
+            map.put("post_id" , postId);
 
-        List<CommentResponseDTO> list = commentService.postDetailComment(map);
+            list = commentService.postDetailComment(map);
 
-        System.out.println("result size = " + list.size());
-        
+            System.out.println("result size = " + list.size());
+        }
         return new ResponseEntity<List<CommentResponseDTO>>(list, HttpStatus.OK);
 
     }
     
     // 댓글 삭제
     @DeleteMapping("/delete/{commentId}")
-    public ResponseEntity<Void> commentDelete(@PathVariable("commentId") Integer commentId,
-                                                @RequestBody CommentRequestDTO request                                          
+    public ResponseEntity<Map<String, Object>> commentDelete(@PathVariable("commentId") Integer commentId,
+                                                @RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader,
+                                                @RequestBody CommentRequestDTO cdto                                      
     ){
+        ResponseEntity<Map<String, Object>> response = null;    
+        Map<String, Object> result = new HashMap<>();              
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7); // "Bearer " 접두사 제거
+            int userId = jwtTokenProvider.getUserIdFromToken(token); // 토큰에서 사용자 ID 추출 (int형으로 변경)
+            System.out.println("userId : " + userId);
       
-        System.out.println("Controller client endpoint : /community/comment/delete/{commentId} : " + commentId);
-        System.out.println("commentId : " + commentId);
-        System.out.println("memberId : " + request.getMember_id());
+            try{
+                System.out.println("Controller client endpoint : /community/comment/delete/{commentId} : " + commentId);
+                System.out.println("commentId : " + commentId);
+                System.out.println("memberId : " + userId);
+                
+                Map<String, Integer> map = new HashMap<>();
+                map.put("comments_id", commentId);
+                map.put("member_id", (Integer)userId);
+                map.put("post_id", cdto.getPost_id());
+                
+                result = commentService.commentDelete(map);
+                if(result.get("result") != null && (boolean)result.get("result")){         
+                    response = ResponseEntity.ok(result);
+                }else if(result.get("Extable") != null && !(boolean)result.get("Extable")){
+                    response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+                }else {
+                    response =  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+                }
+                
+            }catch(Exception e){
 
-        Integer memberId = request.getMember_id();
-        
-        Map<String, Integer> map = new HashMap<>();
-        map.put("comments_id", commentId);
-        map.put("member_id", memberId);
-        
-        commentService.commentDelete(map);
+                System.err.println("오류 발생: " + e.getMessage());
+                e.printStackTrace();
+                result.put("message", "서버에서 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.");
+                response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(result);
 
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+        }else{
+            result.put("message", "로그인이 필요합니다.");
+            response = ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(result);
+        }
+        return response;
     }
-
 }
