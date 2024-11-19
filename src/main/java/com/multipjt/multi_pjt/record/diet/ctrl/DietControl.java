6170bbed14deb503a85aec1a9d9663a6c2c5f7dd
seokhun.han <seokhun0906@gmail.com>
2024-11-19 15:58:59 +0900

    fix : 식단 추천 flow 개선

--- src/main/java/com/multipjt/multi_pjt/record/diet/ctrl/DietControl.java
+++ src/main/java/com/multipjt/multi_pjt/record/diet/ctrl/DietControl.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -154,7 +155,7 @@ public class DietControl {
         }
     }
 
-    // 식단 조회
+    // 특정 식단 조회
     // 일일 식단 조회를 통해 식단 번호를 가지게 됨
     @PostMapping("/get/diet-and-items")
     public ResponseEntity<Object> findDietAndItems(
@@ -201,6 +202,47 @@ public class DietControl {
             return new ResponseEntity<>("인증 실패", HttpStatus.UNAUTHORIZED);
         }
     }
+
+    // 일일 식단 조회
+    @PostMapping("/get/diet")
+    public ResponseEntity<Object> findDiet(
+        @RequestParam(name = "record_date") String record_date,
+        @RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
+        
+        System.out.println("class endPoint >> " + "/record/diet/get/diet");
+    
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            try {
+                String token = authHeader.substring(7); // "Bearer " 접두사 제거
+                Integer member_id = jwtTokenProvider.getUserIdFromToken(token); // 토큰에서 사용자 ID 추출
+                System.out.println("debug >>> member_id : " + member_id);
+                System.out.println("debug >>> token : " + token);
+    
+                Map<String,Object> map = new HashMap<>();
+                map.put("member_id", member_id);
+                map.put("record_date",record_date);
+
+                // 1. 식단 조회
+                List<DietResponseDTO> dietResult = dietService.dietFindAllRow(map);
+                System.out.println("dietResult >> " + dietResult);
+    
+                if (dietResult != null) {
+                    return new ResponseEntity<>(dietResult, HttpStatus.OK);
+                } else {
+                    // 식단 조회 결과가 없을 때
+                    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+                }
+            } catch (Exception e) {
+                // 서버 내부 오류 발생
+                System.out.println("서버 오류 발생: " + e.getMessage());
+                return new ResponseEntity<>("서버 오류 발생", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        } else {
+            // 인증 실패
+            return new ResponseEntity<>("인증 실패", HttpStatus.UNAUTHORIZED);
+        }
+    }
+
 
     // 식단 수정
     // 일일 식단 조회를 통해 식단 번호를 알고 있는 상태
