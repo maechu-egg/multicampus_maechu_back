f195a9c62ca98c35b4dc0da5de4c5296c0787ff7
leeyeonju02 <85239317+leeyeonju02@users.noreply.github.com>
2024-11-21 14:20:11 +0900

    feat: ncp 오브젝트 스토리지 설정

---
+++ src/main/java/com/multipjt/multi_pjt/config/NcpStorageConfig.java
@@ -0,0 +1,36 @@
+package com.multipjt.multi_pjt.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+public class NcpStorageConfig {
+    @Value("${ncp.objectstorage.access-key}")
+    private String accessKey;
+
+    @Value("${ncp.objectstorage.secret-key}")
+    private String secretKey;
+
+    @Value("${ncp.objectstorage.endpoint}")
+    private String endpoint;
+
+    @Value("${ncp.objectstorage.bucket-name}")
+    private String bucketName;
+
+    @Value("${ncp.objectstorage.region}")
+    private String region;
+
+
+    @Bean
+    public AmazonS3Client objectStorageClient() {
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpoint, region))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+                .build();
+    }
+}
